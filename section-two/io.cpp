061054b3ded9a1ec07b83a7d7df789c3497dc4a7
Brian R Reed <b.r.reed@gmail.com>
2022-05-24 20:27:48 -0700

    put all functions aside from main into io.cpp and add declarations to main.cpp
    Signed-off-by: Brian R Reed <b.r.reed@gmail.com>

---
+++ section-two/io.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int addNums(int n1, int n2)
+{
+    int sum;
+    sum = n1 + n2;
+    return sum;
+}
+
+int readNumber()
+{
+    int num;
+    std::cin >> num;
+    return num;
+}
+
+void writeAnswer(int tot_sum)
+{
+    std::cout << "Your numbers add up to: " << tot_sum;
+}
--- section-two/main.cpp
+++ section-two/main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
 
-int addNums(int n1, int n2)
-{
-    int sum;
-    sum = n1 + n2;
-    return sum;
-}
-
-int readNumber()
-{
-    int num;
-    std::cin >> num;
-    return num;
-}
-
-void writeAnswer(int tot_sum)
-{
-    std::cout << "Your numbers add up to: " << tot_sum;
-}
+int readNumber();
+int addNums(int n1, int n2);
+void writeAnswer(int tot_sum);
 
 int main()
 {
